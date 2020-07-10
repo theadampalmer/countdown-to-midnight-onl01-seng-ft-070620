1bfd0e46ba7f757ad8fb533c63d1ac06c20ccd96
Adam Palmer <theadampalmer@gmail.com>
2020-07-10 22:23:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,12 @@
 #write your code here
 
-def countdown_with_sleep(n)
+def countdown_with_sleep(10)
   loop do  
-    puts "#{n} SECOND(S)!"
+    puts "#{10} SECOND(S)!"
     break if n <= 1
     n -= 1
     sleep 2
 end
   "HAPPY NEW YEAR!"
 end
-countdown_with_sleep(10)
+countdown
