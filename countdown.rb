c6ed16548be4d17db9ea457a803f8e2a9d98154e
Adam Palmer <theadampalmer@gmail.com>
2020-07-10 22:28:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,3 +11,14 @@
 #   "HAPPY NEW YEAR!"
 
 # countdown_with_sleep(10)
+
+def countdown_with_sleep(n)
+  loop do  
+    puts "#{n} SECOND(S)!"
+    break if n <= 1
+    n -= 1
+    sleep 5
+end
+  "HAPPY NEW YEAR!"
+end
+countdown_with_sleep(10)
